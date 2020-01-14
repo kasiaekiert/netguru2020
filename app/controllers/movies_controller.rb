a20f589be84854083e89f2e7ff9b4a365a94117c
Kasia <kasia.ekiert@gmail.com>
2020-01-14 20:49:56 +0100

    preload for genres in index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,7 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, only: [:send_info]
 
   def index
-    @movies = Movie.all.limit(7)
-    @movies = Movie.paginate(page: params[:page], per_page: 7)
+    @movies = Movie.preload(:genre).paginate(page: params[:page], per_page: 7)
 
   end
 
