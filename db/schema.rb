488f582cbc8706fb113b7405e8ecf22a7a7d99e0
Kasia <kasia.ekiert@gmail.com>
2020-01-14 13:29:35 +0100

    add user to comments, bootstrap comment section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_113804) do
+ActiveRecord::Schema.define(version: 2020_01_14_112500) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_01_13_113804) do
     t.integer "movie_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "genres", force: :cascade do |t|
